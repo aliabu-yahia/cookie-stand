da3888a099c217ee5680d39624e5e11dde71b525
aliabu-yahia <abuyahiaali21@gmail.com>
2021-02-19 23:19:09 +0200

    changing

--- JS/app.js
+++ JS/app.js
@@ -1,130 +1,142 @@
-/*'use strict';
+'use strict';
 
 function randomNum (min,max){
-  return Math.floor( Math.random()*(max-min+1)+min);
+    return Math.random()*(max-min+1)+min;
 }
+
 let image = document.createElement('img');
-let parent = document.getElementById('shop');
+let parent = document.getElementById('store');
 parent.appendChild(image);
 image.setAttribute('src','images/salmon.png');
 
-let seattleshop={
-    name:'seattle',
-    maxPeop:65,
-    minPeop:23,
-    avaCoc:6.3,
-    hourWork:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
+let hourWork=['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+
+function Store (name,maxPeop,minPeop, avaCoc){
+    this.name=name;
+    this.maxPeop=maxPeop;
+    this.minPeop=minPeop;
+    this.avaCoc=avaCoc;
+    this.numCoc=[];
+    this.total=0
 }
 
+Store.prototype.numCock=function(){
+    for(let i=0;i<hourWork.length;i++){
+        this.numCoc[i]=Math.floor((randomNum(this.minPeop,this.maxPeop))*this.avaCoc); 
+        this.total+=this.numCoc[i];
 
-let header = document.createElement('h1');
-parent.appendChild(header)
-header.textContent=seattleshop.name
-
-let undorderedList =document.createElement('ul');
-parent.appendChild(undorderedList);
-
-let total=0
-console.log(total);
-for(let i=0;i<14;i++){
-    let listItem = document.createElement('li');
-  undorderedList.appendChild(listItem);
-  let Randomnumber= Math.floor( randomNum(seattleshop.maxPeop,seattleshop.minPeop)*seattleshop.avaCoc)
-   total=total+ Randomnumber
-  listItem.textContent =`${seattleshop.hourWork[i]}: ${Randomnumber} cookies. `
-}
-let listmun=document.createElement('li');
-undorderedList.appendChild(listmun);
-listmun.textContent = `Total: ${total}`
-
-let tokyoshop={
-    name:'tokyo',
-    maxPeop:24,
-    minPeop:3,
-    avaCoc:1.2,
-    hourWork:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
+    }
 }
 
+// open head
+let table = document.createElement('table');
+parent.appendChild(table);
 
-let header2 = document.createElement('h1');
-parent.appendChild(header2)
-header2.textContent=tokyoshop.name
+let tr1=document.createElement('tr');
+table.appendChild(tr1);
+  
+let th1=document.createElement('th')
+tr1.appendChild(th1)
+th1.textContent="The Time";
 
-let undorderedList2 =document.createElement('ul');
-parent.appendChild(undorderedList2);
-
-let total2=0
-console.log(total2);
-for(let i=0;i<14;i++){
-    let listItem2 = document.createElement('li');
-  undorderedList2.appendChild(listItem2);
-  let Randomnumber2= Math.floor( randomNum(tokyoshop.maxPeop,tokyoshop.minPeop)*tokyoshop.avaCoc)
-   total2=total2+ Randomnumber2
-  listItem2.textContent =`${tokyoshop.hourWork[i]}: ${Randomnumber2} cookies. `
+for(let i=0;i<hourWork.length;i++){
+    let th=document.createElement('th');
+tr1.appendChild(th);
+th.textContent=hourWork[i];
 }
-let listmun2=document.createElement('li');
-undorderedList2.appendChild(listmun2);
-listmun2.textContent = `Total: ${total2}`
+let th2=document.createElement('th')
+tr1.appendChild(th2)
+th2.textContent="The Total";
 
 
-let dubaishop={
-    name:'dubai',
-    maxPeop:38,
-    minPeop:11,
-    avaCoc:3.7,
-    hourWork:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
-}
+Store.prototype.row=function(){
+    let tr=document.createElement('tr');
+    table.appendChild(tr);
+    let th=document.createElement('th')
+    tr.appendChild(th)
+    th.textContent=this.name;
+    for(let i=0;i<hourWork.length;i++){
+        let td=document.createElement('td');
+        tr.appendChild(td);
+        td.textContent=this.numCoc[i];
 
-
-let header3 = document.createElement('h1');
-parent.appendChild(header3)
-header3.textContent=dubaishop.name
-
-let undorderedList3 =document.createElement('ul');
-parent.appendChild(undorderedList3);
-
-let total3=0
-console.log(total3);
-for(let i=0;i<14;i++){
-    let listItem3 = document.createElement('li');
-  undorderedList3.appendChild(listItem3);
-  let Randomnumber3= Math.floor( randomNum(dubaishop.maxPeop,dubaishop.minPeop)*dubaishop.avaCoc)
-   total3=total3+ Randomnumber3
-  listItem3.textContent =`${dubaishop.hourWork[i]}: ${Randomnumber3} cookies. `
-}
-let listmun3=document.createElement('li');
-undorderedList3.appendChild(listmun3);
-listmun3.textContent = `Total: ${total3}`
-
-
-let parisshop={
-    name:'paris',
-    maxPeop:38,
-    minPeop:11,
-    avaCoc:3.7,
-    hourWork:['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'],
+    }
+    let th1=document.createElement('th')
+    tr.appendChild(th1)
+    th1.textContent=this.total;
 }
 
+let seattle=new Store('Seattle',65,23,6.3);
+//console.log(seattle);
+seattle.numCock();
+seattle.row();
 
-let header4 = document.createElement('h1');
-parent.appendChild(header4)
-header4.textContent=parisshop.name
+let tokyo=new Store('Tokyo',24,3,1.2);
+//console.log(tokyo);
+tokyo.numCock();
+tokyo.row();
 
-let undorderedList4 =document.createElement('ul');
-parent.appendChild(undorderedList4);
 
-let total4=0
-console.log(total4);
-for(let i=0;i<14;i++){
-    let listItem4 = document.createElement('li');
-  undorderedList4.appendChild(listItem4);
-  let Randomnumber4= Math.floor( randomNum(parisshop.maxPeop,parisshop.minPeop)*parisshop.avaCoc)
-   total4=total4+ Randomnumber4
-  listItem4.textContent =`${parisshop.hourWork[i]}: ${Randomnumber4} cookies. `
+let dubai=new Store('Dubai',38,11,3.7);
+//console.log(dubai);
+dubai.numCock();
+dubai.row();
+
+let Paris=new Store('Paris',38,20,2.3);
+//console.log(Paris);
+Paris.numCock();
+Paris.row();
+
+let Lima=new Store('Lima',16,2,4.6);
+//console.log(Lima);
+Lima.numCock();
+Lima.row();
+ 
+let tr2=document.createElement('tr');
+table.appendChild(tr2);
+  
+let th3=document.createElement('th')
+tr2.appendChild(th3)
+th3.textContent="The Total";
+
+for(let i=0;i<hourWork.length;i++){
+    let th=document.createElement('th');
+tr2.appendChild(th);
+th.textContent= seattle.numCoc[i]+tokyo.numCoc[i]+dubai.numCoc[i]+Paris.numCoc[i]+Lima.numCoc[i];
 }
-let listmun4=document.createElement('li');
-undorderedList4.appendChild(listmun4);
-listmun4.textContent = `Total: ${total4}`
-*/
+let th4=document.createElement('th')
+tr2.appendChild(th4)
+th4.textContent=seattle.total+tokyo.total+dubai.total+Paris.total+Lima.total;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
